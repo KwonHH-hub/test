68a469d5e5d3e22ec74e1a1808d510b43b00a0f3
KwonHH-hub <khh202151@gmail.com>
2021-02-25 16:10:15 +0900

    commit

--- x2.py
+++ x2.py
@@ -2,7 +2,7 @@ import os
 print("hello python hello")
 dir = os.getcwd()
 
-f = open(dir + 'myname.txt', 'w')
+f = open(dir + 'myname.txt', 'a')
 x = f.readline()
 if 'Gildong' in x:
     x.replace('Gildong', 'DangMoo')
