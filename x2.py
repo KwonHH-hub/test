20323df9ec0f3d2a788cc6db7875983f101fac1e
KwonHH-hub <khh202151@gmail.com>
2021-02-25 16:53:34 +0900

    commit

--- x2.py
+++ x2.py
@@ -3,16 +3,19 @@ print("hello python hello")
 dir = os.getcwd()
 print(dir)
 f = open('./myname.txt', 'r+')
-
+temp = ""
 while 1:
     line = f.readline()
     print(line)
     if "Gildong" in line:
-        f.write(line.replace("Gildong", "DangMoo"))
+        temp += f.write(line.replace("Gildong", "DangMoo"))
     else:
-        f.write(line)
+        temp += f.write(line)
+    temp += '\n'
     if not line:
         break
+
+
 # f.close()
 # print("i'm here : ", x)
 # if "Gildong" in x:
