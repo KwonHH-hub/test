6bcc0b47a1afce9371451383671126548aff4444
KwonHH-hub <khh202151@gmail.com>
2021-02-25 16:23:57 +0900

    commit

--- x2.py
+++ x2.py
@@ -2,11 +2,11 @@ import os
 print("hello python hello")
 dir = os.getcwd()
 print(dir)
-f = open('./myname.txt', 'r+')
+f = open('./myname.txt', 'w')
 
 x = f.readline()
 print("i'm here : ", x)
 if "Gildong" in x:
     print("if문 들어오니")
-    x.replace("Gildong", "DangMoo", 1)
+    f.write(x.replace("Gildong", "DangMoo"))
 f.close()
