1f2b8b94735cf5da1966e93a85ab3337c4482185
KwonHH-hub <khh202151@gmail.com>
2021-02-25 16:20:17 +0900

    commit

--- x2.py
+++ x2.py
@@ -6,6 +6,7 @@ f = open('./myname.txt', 'r+')
 
 x = f.readline()
 print("i'm here : ", x)
-if 'Gildong' in x:
-    x.replace('Gildong', 'DangMoo')
+if "Gildong" in x:
+
+    x.replace("Gildong", "DangMoo")
 f.close()
