50118b9d1c49d9222754e84fa9cf73664a167511
KwonHH-hub <khh202151@gmail.com>
2021-02-25 16:45:48 +0900

    commit

--- x2.py
+++ x2.py
@@ -2,11 +2,19 @@ import os
 print("hello python hello")
 dir = os.getcwd()
 print(dir)
-f = open('./myname.txt', 'w+')
+f = open('./myname.txt', 'r+')
 
-x = f.readline()
-print("i'm here : ", x)
-if "Gildong" in x:
-    print("if문 들어오니")
-    f.write(x.replace("Gildong", "DangMoo"))
-    f.close()
+while 1:
+    line = f.readline()
+    if "Gildong" in line:
+        f.write(line.replace("Gildong", "DangMoo"))
+    else:
+        f.write(line)
+    if not line:
+        break
+# f.close()
+# print("i'm here : ", x)
+# if "Gildong" in x:
+#     print("if문 들어오니")
+#     f.write(x.replace("Gildong", "DangMoo"))
+#     f.close()
